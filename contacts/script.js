40898151ca4cb1719d221937cda628ab89d94d99
Asel <asel.alimbaeva.kk@gmail.com>
2023-03-22 20:24:37 +0300

    feat: add card and style for card

--- contacts/script.js
+++ contacts/script.js
@@ -38,3 +38,50 @@
       }
   };
 }
+
+{
+    // const contactData = localStorage.getItem('ContactObject');
+
+    class Contacts {
+        constructor () {
+            this.data = JSON.parse(localStorage.getItem('ContactObject'));
+            this.rezultBlock = document.querySelector('.result');
+        }
+
+        showContacts() {
+            if (this.data) {
+                this.data.map((el) => {
+                    console.log(el);
+                    this.rezultBlock.innerHTML += `
+                    <div class="contact-item" data-id="${el.id - 1}">
+                        <div class="text">
+                            <p>Company name: <b>${el.company.name}</b></p>
+                            <p>Name: <b>${el.name}</b></p>
+                            <p>Website: <b>${el.website}</b></p>
+                            <p>City: <b>${el.website}</b></p>
+                            <p>Phone: <b>${el.phone}</b></p>
+                            <button>To learn more</button>
+                        </div>
+                        <div class="btn-block">
+                            <button>
+                                <svg class="">
+                                    <use xlink:href='./assets/icon/sprite.svg#pen'></use>
+                                </svg>
+                            </button>
+                            <button>
+                                <svg class="">
+                                    <use xlink:href='./assets/icon/sprite.svg#marker'></use>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    `
+                })
+            }
+        }
+    }
+
+    const callContacts = new Contacts();
+
+    console.log(callContacts.showContacts());
+}
